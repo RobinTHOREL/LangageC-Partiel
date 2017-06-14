1ec5778eadff55e39678c86d55af0d42f6b46821
brixton <thorelrobin@yahoo.fr>
2017-06-14 14:10:54 +0200

    Second commit

---
+++ cmake-build-debug/CMakeFiles/Partiel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Partiel.dir/main.c.obj"
+  "Partiel.pdb"
+  "Partiel.exe"
+  "Partiel.exe.manifest"
+  "libPartiel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Partiel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
